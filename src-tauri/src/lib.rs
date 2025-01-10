e8874d1b1f392d6ab53ff1b28b2d5c39e849c260
Taylor Nguyen <taylornguyen904@gmail.com>
2025-01-09 17:02:29 -0800

    trying to do directory traversal by myself

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,35 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::collections::HashMap;
+use std::fs::{self, DirEntry};
+use std::{string, vec}; 
+use walkdir::WalkDir;
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 #[tauri::command]
 fn get_book_parts() -> Vec<String> {
-    use std::fs; 
-    let paths = fs::read_dir("./src/story").unwrap();
+    let paths = fs::read_dir("./src/Homer").unwrap();
     let mut paths_string = vec![];
     for path in paths{
         paths_string.push(path.unwrap().path().display().to_string().clone());
     }
     paths_string
+}
+pub fn recursive_directory_traversal() -> Vec<&Vec<&str>>{
+    let start_dir = "./src/Homer";
+    let mut file_contents: Vec<&Vec<&str>> = vec![];
+    for entry in WalkDir::new(start_dir).into_iter().filter_map(|e| e.ok()) {
+        // Print the path of each entry
+        let mut string_vec: Vec<&str> = vec![];
+        let revised_entry: Vec<&str> = entry.path().to_str().unwrap().split("\\").collect();
+        let desired_str: Vec<&str> = revised_entry.splice(1.., []).collect();
+
+        if(entry.path().is_file()){
+            file_contents.push(&desired_str);
+        }
+    file_contents
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    homer_lib::run()
+    //homer_lib::run()
+    homer_lib::recursive_directory_traversal();
 }
