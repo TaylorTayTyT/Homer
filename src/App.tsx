b20a18bbfdb332de728b5a51e969c4a647809022
Taylor Nguyen <taylornguyen904@gmail.com>
2025-01-27 14:32:38 -0800

    fixed the fullscreen glitch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import TextEditor from "./Editor";
 import { invoke } from "@tauri-apps/api/core";
 import FileExplorer from "./FileExplorer";
 import "./Styles/App.css"
-import { useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import FullscreenOptions from "./FullScreenOptions";
 export default function App() {
     const [isFullscreen, SetIsFullScreen] = useState(false);
@@ -12,14 +12,48 @@ export default function App() {
         const sidebar = document.getElementById("sidebar");
         if(!sidebar) return; 
         isFullscreen ? sidebar.style.position = "absolute" : sidebar.style.position = "relative";
-    }, [isFullscreen])
+    }, [isFullscreen]);
+    useEffect(()=>{
+        function sidebarDrag (e: MouseEvent) {
+            console.log(e.clientX)
+        };
+        function editorDrag(e: MouseEvent){
+            console.log(e.clientX)
+        }
+        document.getElementById("sidebar")?.addEventListener("mousedown", sidebarDrag);
+        document.getElementById("sidebar")?.addEventListener("mouseover", (e) =>{
+            const x = document.getElementById("sidebar")?.getBoundingClientRect().right;
+            if(!x) return;
+            console.log(x)
+            console.log(e.clientX)
+            if(x - e.clientX < 16){
+                console.log("valid")
+            }
+            //START WORK HERE
+        });
+        document.getElementById("drag")?.addEventListener("mousedown", editorDrag);
+    }, [])
+
+    type FullScreenToggleProps = {
+      SetIsFullScreen: React.Dispatch<React.SetStateAction<boolean>>;
+      editorRef: any;
+    };
+
+    //use this so you dont rerender the text editor
+    const Control = memo(function Control(){
+        return (
+            <TextEditor/>
+        )
+    });
     return (
         <div id="container" data-fullscreen={isFullscreen}>
             <div id="sidebar">
-                {isFullscreen ? <FullscreenOptions editorRef={editorRef} /> : <FileExplorer />}
+                <div>
+                {isFullscreen ? "" : <FileExplorer />}
+                </div>
             </div>
             <div id="text_editor">
-                <TextEditor SetIsFullScreen={SetIsFullScreen} editorRef={editorRef}/>
+                <Control/>
             </div>
         </div>
     )
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -8,7 +8,7 @@ type FullScreenToggleProps = {
 };
 
 
-export default function TextEditor({ SetIsFullScreen, editorRef }: FullScreenToggleProps) {
+export default function TextEditor() {
   // const log = () => {
   //   if (editorRef.current) {
   //     console.log(editorRef.current.getContent());
@@ -17,15 +17,11 @@ export default function TextEditor({ SetIsFullScreen, editorRef }: FullScreenTog
 
   const tinyMCECSS = 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px } #tinymce{margin-left: 10%;width: 80%;} .tox-tinymce:focus {outline: none !important;box-shadow: none !important;}';
   return (
-    <><button style={{zIndex: 10000, right: 0, position: "absolute"}} onClick={() => {
-      editorRef.current.execCommand("mceFullScreen");
-    }}>fdfs</button>
+    <>
       <div id='editor' aria-hidden="false">
         <Editor
-          key={editorRef.current ? 'editor-mounted' : 'editor-unmounted'}
           apiKey='2j6d39ibmyedqmngituet4cu6t6itq6p5ipd319un1patjk8'
           id="editor"
-          onInit={(evt, editor) => (editorRef.current = editor)}
           initialValue="<p>This is the initial content of the editor.</p>"
           init={{
             setup: (editor) => {
@@ -41,14 +37,16 @@ export default function TextEditor({ SetIsFullScreen, editorRef }: FullScreenTog
               });
               editor.ui.registry.addIcon('focus', QuillIcon);
               editor.on('FullscreenStateChanged', (e) => {
-                document.querySelector("div[role=menubar]")?.classList.toggle("disable");
-                document.querySelectorAll("div[role=toolbar]").forEach((tool) => {
-                  tool.classList.toggle("disable");
-                });
-                document.querySelectorAll("div[role=toolbar] div").forEach((tool) => {
-                  tool.classList.toggle("disable");
+                document.querySelectorAll("div[role=menubar]")?.forEach(menubar =>{
+                  menubar.classList.toggle("disable");
                 })
-                SetIsFullScreen(e.state)
+                document.querySelectorAll("div[role=toolbar]")?.forEach(toolbar =>{
+                  if(toolbar.querySelector("button[data-mce-name=\"focus\"")){
+                    return; 
+                  } else {
+                    toolbar.classList.toggle("disable");
+                  }
+                })
               })
             },
             height: window.innerHeight,
@@ -73,7 +71,7 @@ export default function TextEditor({ SetIsFullScreen, editorRef }: FullScreenTog
               'help',
               'wordcount',
             ],
-            toolbar: 'focus undo redo | formatselect | bold italic backcolor | fontselect fontsizeselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+            toolbar: 'focus | undo redo | formatselect | bold italic backcolor | fontselect fontsizeselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
             font_formats: 'Arial=arial,helvetica,sans-serif; Times New Roman=times new roman,times; Verdana=verdana,geneva;',
             fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
             content_style: tinyMCECSS,
