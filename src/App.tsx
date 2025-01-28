4caf6ce1e0558e6d691e80e44f8e7f43a613fcec
Taylor Nguyen <taylornguyen904@gmail.com>
2025-01-27 16:49:38 -0800

    have a prototype of the slider

--- src/App.tsx
+++ src/App.tsx
@@ -7,53 +7,64 @@ import FullscreenOptions from "./FullScreenOptions";
 export default function App() {
     const [isFullscreen, SetIsFullScreen] = useState(false);
     const editorRef = useRef();
-    useEffect(()=>{
-        editorRef.current = undefined; 
+    useEffect(() => {
+        editorRef.current = undefined;
         const sidebar = document.getElementById("sidebar");
-        if(!sidebar) return; 
+        if (!sidebar) return;
         isFullscreen ? sidebar.style.position = "absolute" : sidebar.style.position = "relative";
     }, [isFullscreen]);
-    useEffect(()=>{
-        function sidebarDrag (e: MouseEvent) {
-            console.log(e.clientX)
-        };
-        function editorDrag(e: MouseEvent){
-            console.log(e.clientX)
-        }
-        document.getElementById("sidebar")?.addEventListener("mousedown", sidebarDrag);
-        document.getElementById("sidebar")?.addEventListener("mouseover", (e) =>{
-            const x = document.getElementById("sidebar")?.getBoundingClientRect().right;
-            if(!x) return;
-            console.log(x)
-            console.log(e.clientX)
-            if(x - e.clientX < 16){
-                console.log("valid")
+    useEffect(() => {
+        function sidebarDrag(e: MouseEvent) {
+            const initX = e.clientX; 
+            const sidebar = document.getElementById("sidebar");
+            if(!sidebar) return; 
+            const sideBarWidth = sidebar.getBoundingClientRect().width;
+            console.log(initX)
+            function adjust(e: MouseEvent){
+                if(!sidebar) return;
+                let newWidth = sideBarWidth - (initX - e.clientX)
+                sidebar.style.width = `${e.clientX}px`;
             }
-            //START WORK HERE
-        });
-        document.getElementById("drag")?.addEventListener("mousedown", editorDrag);
+            sidebar?.addEventListener("mousemove", adjust);
+            sidebar?.addEventListener('mouseup', (e) =>{
+                sidebar.removeEventListener("mousemove", adjust);
+            })
+        };
+
+        const sidebar = document.getElementById("sidebar");
+
+        if (sidebar) {
+            sidebar.addEventListener("mousedown", sidebarDrag);
+            sidebar.addEventListener("mousemove", (e)=>{
+                if(sidebar.getBoundingClientRect().right - e.clientX < 16) sidebar.style.cursor = "col-resize"
+                else{
+                    sidebar.style.cursor = "";
+                }
+            })
+        }
+
     }, [])
 
     type FullScreenToggleProps = {
-      SetIsFullScreen: React.Dispatch<React.SetStateAction<boolean>>;
-      editorRef: any;
+        SetIsFullScreen: React.Dispatch<React.SetStateAction<boolean>>;
+        editorRef: any;
     };
 
     //use this so you dont rerender the text editor
-    const Control = memo(function Control(){
+    const Control = memo(function Control() {
         return (
-            <TextEditor/>
+            <TextEditor />
         )
     });
     return (
         <div id="container" data-fullscreen={isFullscreen}>
             <div id="sidebar">
                 <div>
-                {isFullscreen ? "" : <FileExplorer />}
+                    {isFullscreen ? "" : <FileExplorer />}
                 </div>
             </div>
             <div id="text_editor">
-                <Control/>
+                <Control />
             </div>
         </div>
     )
