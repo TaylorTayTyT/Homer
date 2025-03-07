f8de115c70574c9d45a3c348b85e1b968f4e66f9
Taylor Nguyen <taylornguyen904@gmail.com>
2025-03-07 14:28:53 -0800

    codemirror integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,62 +7,81 @@ import Timeline from "./Timeline";
 import FullscreenOptions from "./FullScreenOptions";
 import { JSX } from "react";
 import Instructions from "./Instructions";
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+import ReactCodeMirror from "@uiw/react-codemirror";
+
 export default function App() {
     const [isFullscreen, SetIsFullScreen] = useState(false);
     const [activeFile, SetActiveFile] = useState<string>();
     const [activeFileHTML, SetActiveFileHTML] = useState<Element>(); 
     const [type, SetType] = useState<string>();
     const activeFileRef = useRef(activeFileHTML);
-    const [editor, SetEditor] = useState<JSX.Element>(<></>); 
+    
+    const [value, SetValue] = useState("");
+
+    function editorChanges(value: string){
+        SetValue(value);
+    }
 
     useEffect(() => {
         const sidebar = document.getElementById("sidebar");
         if (!sidebar) return;
         isFullscreen ? sidebar.style.position = "absolute" : sidebar.style.position = "relative";
     }, [isFullscreen]);
+
+    // handles the file explorer resize
+    function sidebarDrag() {
+        const sidebar = document.getElementById("sidebar");
+        
+        if(!sidebar) return; 
+        let barrier = document.querySelector("li[data-first=true] strong div.arrow")?.getBoundingClientRect().right; 
+        function adjust(e: MouseEvent){
+            if(!sidebar) return; 
+            let newWidth = e.clientX
+            if(!barrier || newWidth < barrier) return; 
+            sidebar.style.width = `${newWidth}px`;
+        }
+        document.addEventListener("mousemove", adjust);
+        document.addEventListener('mouseup', (e) =>{
+            document.removeEventListener("mousemove", adjust);
+        })
+    };
+    //makes sure you have pointer when resizing
+    function addCursor(e: MouseEvent){
+        //START HERE FIND OUT HOW TO ADD THE CURSOR
+        const sidebar = document.getElementById("sidebar");
+        if(!sidebar) return; 
+        sidebar.addEventListener("mousemove", (e: MouseEvent) =>{
+            const r = sidebar?.getBoundingClientRect().right
+            if(r - e.x < 16) sidebar.style.cursor = "col-resize";
+            else removeCursor(e)
+        })
+        
+    }
+    //removes the pointer when done resizing
+    function removeCursor(e: MouseEvent){
+        const sidebar = document.getElementById("sidebar");
+        if(!sidebar) return; 
+        sidebar.style.cursor = "auto";
+    }
+
     useEffect(() => {
-        // handles the file explorer resize
-        function sidebarDrag(e: MouseEvent) {
-            const sidebar = document.getElementById("sidebar");
-            
-            if(!sidebar) return; 
-            let barrier = document.querySelector("li[data-first=true] strong div.arrow")?.getBoundingClientRect().right; 
-            function adjust(e: MouseEvent){
-                if(!sidebar) return; 
-                let newWidth = e.clientX
-                if(!barrier || newWidth < barrier) return; 
-                sidebar.style.width = `${newWidth}px`;
-            }
-            document.addEventListener("mousemove", adjust);
-            document.addEventListener('mouseup', (e) =>{
-                document.removeEventListener("mousemove", adjust);
-            })
-        };
-
-        function addCursor(e: MouseEvent){
-            //START HERE FIND OUT HOW TO ADD THE CURSOR
-            const sidebar = document.getElementById("sidebar");
-            if(!sidebar) return; 
-            sidebar.addEventListener("mousemove", (e: MouseEvent) =>{
-                const r = sidebar?.getBoundingClientRect().right
-                if(r - e.x < 16) sidebar.style.cursor = "col-resize";
-                else removeCursor(e)
-            })
-            
-        }
-
-        function removeCursor(e: MouseEvent){
-            const sidebar = document.getElementById("sidebar");
-            if(!sidebar) return; 
-            sidebar.style.cursor = "auto";
-        }
-
         const sidebar = document.getElementById("sidebar");
         if (sidebar) {
             sidebar.addEventListener("mouseover", addCursor)
             sidebar.addEventListener("mousedown", sidebarDrag);
         }
 
+        let startState = EditorState.create({
+            doc: "Hello World",
+            extensions: [keymap.of(defaultKeymap)]
+        });
+        let view = new EditorView({
+            state: startState, 
+            parent: document.getElementById("editor") as HTMLElement //forces there to be an element with id editor
+        })
     }, []);
 
     useEffect(()=>{
@@ -77,7 +96,7 @@ export default function App() {
                 </div>
             </div>
             <div id="text_editor">
-                {type === "timeline" ? <Timeline/> : type ? <TextEditor type={type} activeFile={activeFile} activeFileHTML={activeFileHTML} activeFileRef={activeFileRef}/>  : <Instructions/>}
+                {type === "timeline" ? <Timeline/> : type ? <ReactCodeMirror value="hello" height="200px" onChange={editorChanges}/>  : <Instructions/>}
             </div>
         </div>
     )
